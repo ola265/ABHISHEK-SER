d4503a380b574f4013577a1034f11e2d3e4b1b8d
ola265 <68593577+ola265@users.noreply.github.com>
2023-08-19 01:50:18 +0100

    Update prefix.js

--- plugins/prefix.js
+++ plugins/prefix.js
@@ -1,10 +1,10 @@
 let handler = async(m, { conn, text }) => {
   if (!text) throw `No symbol detected ...`
-  global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
+  global.prefix = new RegExp('^[' + (text || global.opts['!'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
   await m.reply(`the prefix has been changed to  *${text}*`)
 }
-handler.help = ['setprefix'].map(v => v + ' [prefix]')
-handler.tags = ['owner']
+handler.help = ['setprefix'].map(v => v + ' [!]')
+handler.tags = ['VIRUS']
 handler.command = /^(setprefix)$/i
 handler.rowner = true
 
