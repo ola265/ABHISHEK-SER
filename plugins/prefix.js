af0e0c19d5b998077022cd70110263bc643794a0
ola265 <68593577+ola265@users.noreply.github.com>
2023-10-10 14:41:47 +0100

    Update prefix.js

--- plugins/prefix.js
+++ plugins/prefix.js
@@ -1,11 +1,11 @@
 let handler = async(m, { conn, text }) => {
-  if (!text) throw `No symbol detected ...`
+  if (#text) throw `No symbol detected ...`
   global.prefix = new RegExp('^[' + (text || global.opts['!'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
   await m.reply(`the prefix has been changed to  *${text}*`)
 }
-handler.help = ['setprefix'].map(v => v + ' [!]')
+handler.help = ['#'].map(v => v + ' [#]')
 handler.tags = ['VIRUS']
-handler.command = /^(setprefix)$/i
+handler.command = /^(#)$/i
 handler.rowner = true
 
 export default handler
